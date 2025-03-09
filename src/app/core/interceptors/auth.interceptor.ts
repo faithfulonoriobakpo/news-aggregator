e5022799f3b6b6f33533e98ad70c3e7293c24da9
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2025-03-09 20:15:56 +0100

    updated api key

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const authToken = 'c5bf185b9acf402694485692e9dfd636';
+  const authToken = 'c1864c2213c84ebbaedcbc022b3a882b';
 
   const authReq = req.clone({
     setHeaders: {
